cb3b5204e6af46eb57759ed00fe5d565b9bf019d
kohei240155 <k.pino519@gmail.com>
2024-06-09 15:14:38 +0900

    Add MainVisual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from './components/Header';
+import MainVisual from "./components/MainVisual";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <MainVisual />
     </div>
   );
 }
